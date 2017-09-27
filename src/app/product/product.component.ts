3c6d0df7bff12fe823d775bb06af1b4c6e265c2c
boo-boom <510126766@qq.com>
2017-09-27 16:47:05 +0800

    first

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ){}
+}
