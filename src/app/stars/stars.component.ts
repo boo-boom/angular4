8b4dc220659fc84ac461839fbba8e7513a3d6862
boo-boom <510126766@qq.com>
2017-10-11 17:54:44 +0800

    @input

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
-  styleUrls: ['./stars.component.css']
+  styleUrls: ['./stars.component.scss']
 })
 export class StarsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  rating = 0;
+
+  stars: boolean[];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating);
+    }
   }
 
 }
